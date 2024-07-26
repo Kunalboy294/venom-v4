60867874610d6b976c262f2fd7eb9dbe45d54896
Kunalboy294 <kunaltikadar07@gmail.com>
2024-07-26 16:24:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6485558034:AAHavkh_Izcgtj-Pzs3D7MYRAKzHzdKlgcc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("prince boy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7245486488")
 
 
 
